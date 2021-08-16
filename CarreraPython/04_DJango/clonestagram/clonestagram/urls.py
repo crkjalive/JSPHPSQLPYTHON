85f98ba93c3791c6e3d54ad59c286233acaafaa1
crkj <0soundlive@gmail.com>
2021-08-15 21:44:39 -0500

    borrado de todo lo que hicimos

--- CarreraPython/04_DJango/clonestagram/clonestagram/urls.py
+++ CarreraPython/04_DJango/clonestagram/clonestagram/urls.py
@@ -1,8 +1,14 @@
+# Django
+from django.contrib import admin
 from django.urls import path
+
+# mis utilities
 from clonestagram import views as local_views
 from posts import views as posts_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+
     path('hellow-world/', local_views.hellow_world),
     path('sorted/', local_views.sorted_numeros),
     path('hi/<str:name>/<int:age>/', local_views.hi),
